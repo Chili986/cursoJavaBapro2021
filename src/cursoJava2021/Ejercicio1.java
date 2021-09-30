806bb5a0a2b30195d77188913edbed24c8b23572
Chili986 <56000631+Chili986@users.noreply.github.com>
2021-09-30 10:00:26 -0300

    Initial commit

---
+++ src/cursoJava2021/Ejercicio1.java
@@ -0,0 +1,11 @@
+package cursoJava2021;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n\tNombre\tApellido\temail\t\t\ttelefono");
+		System.out.println("\n\tTeresa\tDelgado\t\ttndelgado@bpba.cpm\t1535587268");
+	}
+
+}
