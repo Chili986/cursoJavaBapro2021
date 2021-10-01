805341c193d2030300f015df246eaa8e1e0814ab
Chili986 <56000631+Chili986@users.noreply.github.com>
2021-10-01 08:33:15 -0300

    Clase3

---
+++ src/cursoJava2021/Ejercicio2.java
@@ -0,0 +1,15 @@
+package cursoJava2021;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n\tNombre\t\tApellido\temail\t\t\ttelefono");
+		System.out.println("\t-------\t\t-------\t\t-------\t\t\t--------");
+		System.out.println("\n\tTeresa\t\tDelgado\t\ttndelgado@bpba.com\t1535587268");
+		System.out.println("\tTatiana\t\tMaqueira\tMaqueira@bpba.com\t1535867268");
+		System.out.println("\tVeronica\tCastro\t\tvcastro@bpba.com\t1655895268");
+
+	}
+
+}
---
+++ src/cursoJava2021/Ejercicio3.java
@@ -0,0 +1,17 @@
+package cursoJava2021;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n\tTecla de escape\t\t\tSignificado");
+		System.out.println("\n\t\\n\t\t\t\t"+"Significa nueva línea");
+		System.out.println("\t\\t \t\t\t\tSignifica un tab de espacio");
+		System.out.println("\t\\”\t\t\t\tEs para poner “ (comillas dobles) dentro del texto por ejemplo"+" "+ "“Belencita”");
+		System.out.println("\t\\\\\t\t\t\tSe utiliza para escribir la \\ dentro del texto, por ejemplo \\algo\\");
+		System.out.println("\t\\’ \t\t\t\tSe utiliza para las ‘(comilla simple) para escribir por ejemplo ‘Princesita’");
+		
+		
+	}
+ 
+}
