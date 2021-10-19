c7506e91eeb03939e16fd2e6f4853327b202c707
Chili986 <56000631+Chili986@users.noreply.github.com>
2021-10-18 21:24:07 -0300

    tablas

--- src/Modulo4/SumaDePaloresPares.java
+++ src/Modulo4/SumaDePaloresPares.java
@@ -6,13 +6,34 @@ public class SumaDePaloresPares {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		Scanner sc = new Scanner(System.in);   
-		
-		int i = sc.nextInt();
-		
-		if (i =< 16 )
+		System.out.println("Tabla de:");
+	  	Scanner sc = new Scanner (System.in);
+	       int num = sc.nextInt();
+		  	
+      		
+
+	    	for (int i = 1;i<=10 ;i ++) {
+	    	
+	    		if(i % 2 == 0)
+	    			
+	    	 System.out.println(num + "x" + i + "=" +(num*i) );
+	       	 
+	    	}
+	    	
+	    	System.out.println(" Otra tabla de:");
+	    	  int num1 = sc.nextInt();
+	    	  
+	    	for (int i = 1;i<=10 ;i = i + 2) {
+		    	
+	    			
+	    	 System.out.println(num1 + "x" + i + "=" +(num1*i) );
+	       	 
+	    	}
+	    	  
+	    	 	
 			
+   		   
+	
     		   
 	}
 
